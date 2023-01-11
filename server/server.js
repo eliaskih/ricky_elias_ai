4c6a0454e265ec521e4b4cb89825f5a4b06f6256
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-01-11 17:02:54 +0100

    yo

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post('/', async (req, res) => {
         frequency_penalty: 0.5,
         presence_penalty: 0,
     });
-    
+
     res.status(200).send({
         bot: response.data.choices[0].text
     })
@@ -44,4 +44,4 @@ app.post('/', async (req, res) => {
  }
 })
 
-app.listen(5000, () => console.log('server is running on port http://localhost:5000'));
+app.listen(process.env.PORT, () => console.log('server is running on port http://localhost:5000'));
